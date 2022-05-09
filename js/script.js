b67cb6a3e46f581e8ca6f4636ea1fbc789654f47
Pietroarmill <pietroarmillotta38@gmail.com>
2022-05-09 17:59:49 +0200

    add todo

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const app = new Vue(
+    {
+        el: "#root",
+        data: {
+            todos: [
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    text: "Portare il cane fuori",
+                    done: true
+                },
+                {
+                    text: "Mettere il mangiare al cane",
+                    done: false
+                },
+                {
+                    text: "Lavare l'auto",
+                    done: true
+                },
+            ],
+            newTodo: '',
+
+        },
+        methods: {
+           gestisciClick: function(index) {
+               console.log('click su elemento di indixce ', index);
+               this.todos[index].done = !this.todos[index].done;
+           },
+           addTodo: function() {
+                this.todos.push(
+                        {
+                            text: this.newTodo,
+                            done: false
+                        }
+                    
+                    );
+                this.newTodo = '';
+           }
+        },
+    }
+)
